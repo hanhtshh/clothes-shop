544d1dc83c23e41b84ff0ac5a92b8d43b5478157
Nguyen Van Hanh <nguyenhanh3@telio.vn>
2023-05-08 22:55:41 +0700

    add paypal

--- src/containers/cartcheckout/cartcheckout.js
+++ src/containers/cartcheckout/cartcheckout.js
@@ -5,91 +5,143 @@ import { useState } from "react";
 import { refreshcart } from "../../shared/store/actions";
 import { useHistory } from "react-router-dom";
 import { SERVER } from "../../util";
-export default function CartCheckoutContainer(props){
-    const dispatch=useDispatch();
-    const history=useHistory();
-    const user=useSelector(state=>state.user);
-    const cart=useSelector(state=>state.cart);
-    const [success,setSuccess]=useState(false);
-    const [fall,setFall]=useState(false);
-    const [name,setName]=useState(user.name);
-    const [email,setEmail]=useState(user.email);
-    const [telephone,setTelephone]=useState(user.telephone);
-    const [address,setAddress]=useState(user.address);
-    const handleName=(item)=>{
+export default function CartCheckoutContainer(props) {
+    const dispatch = useDispatch();
+    const history = useHistory();
+    const user = useSelector(state => state.user);
+    const cart = useSelector(state => state.cart);
+    const [success, setSuccess] = useState(false);
+    const [paypalSuccess, setPaypalSuccess] = useState(false);
+    const [fall, setFall] = useState(false);
+    const [name, setName] = useState(user.name);
+    const [email, setEmail] = useState(user.email);
+    const [telephone, setTelephone] = useState(user.telephone);
+    const [address, setAddress] = useState(user.address);
+    async function createOrder(data) {
+        // Order is created on the server and the order id is returned
+        try {
+            const response = await fetch(`${SERVER}/create-paypal-order`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                // use the "body" param to optionally pass additional order information
+                // like product skus and quantities
+                body: JSON.stringify({
+                    cart: [
+                        {
+                            sku: "YOUR_PRODUCT_STOCK_KEEPING_UNIT",
+                            quantity: "YOUR_PRODUCT_QUANTITY",
+                        },
+                    ],
+                }),
+            })
+                .then((response) => response.json())
+                .then((order) => order.id);
+            return response
+        }
+        catch (error) {
+            console.log(error)
+        }
+    }
+    async function onApprove(data) {
+        // Order is captured on the server
+        try {
+            const response = await fetch(`${SERVER}/capture-paypal-order`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    orderID: data.orderID
+                })
+            })
+                .then((res) => res.json())
+            console.log(response);
+            if (response.status === "COMPLETED") {
+                setPaypalSuccess(true);
+            }
+            return response
+        }
+        catch (error) {
+            console.log(error)
+        }
+
+    }
+    const handleName = (item) => {
         setName(item.target.value);
     }
-    const handleEmail=(item)=>{
+    const handleEmail = (item) => {
         setEmail(item.target.value);
     }
-    const handleTelephone=(item)=>{
+    const handleTelephone = (item) => {
         setTelephone(item.target.value);
     }
-    const handleAddress=(item)=>{
+    const handleAddress = (item) => {
         setAddress(item.target.value);
     }
-    const tongtien=()=>{
-        let tong=0;
-        for(let i of cart){
-            tong+=i.item.sale*i.number
+    const tongtien = () => {
+        let tong = 0;
+        for (let i of cart) {
+            tong += i.item.sale * i.number
         }
         return tong;
     }
-    const dathang=()=>{
-        if(user.admin==-1){
+    const dathang = () => {
+        if (user.admin == -1) {
             alert("Bạn cần phải đăng nhập để thanh toán!");
         }
-        else{
-            const date=new Date();
-            axios.post(SERVER+'/oder',{
-                telephone:telephone,
-                address:address,
-                cost:tongtien(),
-                oder_date:(date.getDay()+1)+"/"+(date.getMonth()+1)+"/"+date.getFullYear(),
-                oder_list:cart.map(item=>{
+        else {
+            const date = new Date();
+            axios.post(SERVER + '/oder', {
+                telephone: telephone,
+                address: address,
+                cost: tongtien(),
+                oder_date: (date.getDay() + 1) + "/" + (date.getMonth() + 1) + "/" + date.getFullYear(),
+                oder_list: cart.map(item => {
                     return {
-                        item:item.item._id,
-                        quantity:item.number,
-                        size:item.size
+                        item: item.item._id,
+                        quantity: item.number,
+                        size: item.size
                     }
                 })
-            },{
-                headers:{
-                    Authorization:localStorage.getItem("token")
+            }, {
+                headers: {
+                    Authorization: localStorage.getItem("token")
                 }
             })
-            .then(item=>{
-                axios.patch(SERVER+'/customer/handleInformation',{
-                    name:name,
-                    address:address,
-                    telephone:telephone,
-                    email:email
-                },{
-                    headers:{
-                        Authorization:localStorage.getItem("token")
-                    }
+                .then(item => {
+                    axios.patch(SERVER + '/customer/handleInformation', {
+                        name: name,
+                        address: address,
+                        telephone: telephone,
+                        email: email
+                    }, {
+                        headers: {
+                            Authorization: localStorage.getItem("token")
+                        }
+                    })
+                        .then(e => {
+                            setSuccess(true);
+                            dispatch(refreshcart({
+                                name: name,
+                                address: address,
+                                telephone: telephone,
+                                email: email
+                            }));
+
+                        })
                 })
-                .then(e=>{
-                    setSuccess(true);
-                    dispatch(refreshcart({
-                        name:name,
-                        address:address,
-                        telephone:telephone,
-                        email:email
-                    }));
-                    
+                .catch(item => {
+                    setFall(true);
                 })
-            })
-            .catch(item=>{
-                setFall(true);
-            })
         }
     }
-    const redirect=()=>{
-       history.push("/");
+    const redirect = () => {
+        history.push("/");
     }
-    const displayFall=()=>{
+    const displayFall = () => {
         setFall(false);
     }
-    return <CartCheckoutComponent fall={fall} setFall={displayFall} redirect={redirect} success={success} dathang={dathang} tongtien={tongtien} handleAddress={handleAddress} address={address} handleTelephone={handleTelephone} telephone={telephone} handleEmail={handleEmail} email={email} handleName={handleName} name={name} user={user} cart={cart}/>
+    return <CartCheckoutComponent paypalSuccess={paypalSuccess} createOrder={createOrder} onApprove={onApprove} fall={fall} setFall={displayFall} redirect={redirect} success={success} dathang={dathang} tongtien={tongtien} handleAddress={handleAddress} address={address} handleTelephone={handleTelephone} telephone={telephone} handleEmail={handleEmail} email={email} handleName={handleName} name={name} user={user} cart={cart} />
 }
--- src/containers/cartcheckout/component/cartcheckout.js
+++ src/containers/cartcheckout/component/cartcheckout.js
@@ -1,23 +1,24 @@
+import { PayPalButtons, PayPalScriptProvider } from "@paypal/react-paypal-js";
 import "./cartcheckout.css";
 import { Link } from "react-router-dom";
-export default function CartCheckoutComponent(props){
+export default function CartCheckoutComponent(props) {
     return <div className="cartckeckout">
         <div className="cartcheckout__info">
             <div className="cartcheckout__info-header">
                 <Link to="/cart" className="cartcheckout__info-header-link">Giỏ hàng</Link><i class="fa-solid fa-angle-right"></i>
                 <span className="cartcheckout__info-header-text">Thanh toán</span>
             </div>
-            <p style={{fontSize:"20px"}}>Thông tin giao hàng</p>
+            <p style={{ fontSize: "20px" }}>Thông tin giao hàng</p>
             <form>
-                <input placeholder="Họ và tên" value={props.name!="trong"?props.name:""} onChange={props.handleName} className="cartcheckout__info-name">
+                <input placeholder="Họ và tên" value={props.name != "trong" ? props.name : ""} onChange={props.handleName} className="cartcheckout__info-name">
                 </input>
                 <div className="cartcheckout__info-emailandphone">
-                    <input placeholder="Email" value={props.email!="trong"?props.email:""} onChange={props.handleEmail} className="cartcheckout__info-email">
+                    <input placeholder="Email" value={props.email != "trong" ? props.email : ""} onChange={props.handleEmail} className="cartcheckout__info-email">
                     </input>
-                    <input placeholder="Số điện thoại" onChange={props.handleTelephone} value={props.telephone!="trong"?props.telephone:""} className="cartcheckout__info-telephone">
+                    <input placeholder="Số điện thoại" onChange={props.handleTelephone} value={props.telephone != "trong" ? props.telephone : ""} className="cartcheckout__info-telephone">
                     </input>
                 </div>
-                <input placeholder="Địa chỉ" onChange={props.handleAddress} value={props.address!="trong"?props.address:""} className="cartcheckout__info-address">
+                <input placeholder="Địa chỉ" onChange={props.handleAddress} value={props.address != "trong" ? props.address : ""} className="cartcheckout__info-address">
                 </input>
                 <textarea placeholder="Ghi chú" className="cartcheckout__info-comment">
 
@@ -27,7 +28,7 @@ export default function CartCheckoutComponent(props){
         <div className="cartcheckout__cart">
             <div className="cartcheckout__cart-listitem">
                 {
-                    props.cart.map(item=>{
+                    props.cart.map(item => {
                         return <div className="cartcheckout__cart-item">
                             <div className="cartcheckout__cart-imagebox">
                                 <img className="cartcheckout__cart-image" src={item.item.image[0]}>
@@ -36,11 +37,11 @@ export default function CartCheckoutComponent(props){
                             </div>
                             <div className="cartcheckout__cart-iteminfo">
                                 {item.item.name}
-                                <br/>
+                                <br />
                                 Size: {item.size}
-                                <br/>
+                                <br />
                                 Số lượng: {item.number}
-                                <br/>
+                                <br />
                                 Giá: {item.item.sale.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1.")}₫
                             </div>
                         </div>
@@ -48,37 +49,51 @@ export default function CartCheckoutComponent(props){
                 }
             </div>
             <div className="cartcheckout__cart-ship">
-                Phí vận chuyển <p className="cartcheckout__cart-shipprice">{props.tongtien()>=500000?"---":"30.000₫"}</p>
+                Phí vận chuyển <p className="cartcheckout__cart-shipprice">{props.tongtien() >= 500000 ? "---" : "30.000₫"}</p>
             </div>
             <div className="cartcheckout__cart-ship">
-                Tổng cộng <p className="cartcheckout__cart-shipprice"><b>{props.tongtien().toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1.")}₫</b></p> 
+                Tổng cộng <p className="cartcheckout__cart-shipprice"><b>{props.tongtien().toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1.")}₫</b></p>
             </div>
+            {
+                props.paypalSuccess === true && <div className="cartcheckout__cart-ship">
+                    Đã thanh toán    <i class="fa-solid fa-circle-check" style={{fontSize:"15px"}}></i>
+                </div>
+            }
+            <PayPalScriptProvider options={{ "client-id": "Ac54Kq6hRYMFO6FmD8FA2mYfemDgABSJQZSPsVImkOrEdaNnnN43BWU_zvIpa3piou3A7KYn4sOHJMro" }}>
+                <PayPalButtons
+                    createOrder={(data, actions) => props.createOrder(data)}
+                    onApprove={(data, actions) => props.onApprove(data)}
+                    className="cartchekout__cart-buttonthanhtoan paypal"
+
+                />
+            </PayPalScriptProvider>
             <div className="cartchekout__cart-buttonthanhtoan" onClick={props.dathang}>
                 HOÀN TẤT ĐƠN HÀNG
             </div>
+
         </div>
-        {props.success?<div className="blur">
+        {props.success ? <div className="blur">
             <div className="cartcheckout__successshow">
-            <i class="fa-solid fa-circle-check"></i>
-            <h3>Thành công!</h3>
-            <p>Bạn đã đặt hàng thành công, vui lòng quay lại trang chủ để tiếp tục mua sắm</p>
-            <p>Đơn hàng sẽ được gửi đi trong thời gian sớm nhất, xin cảm ơn!</p>
-            <br/>
+                <i class="fa-solid fa-circle-check"></i>
+                <h3>Thành công!</h3>
+                <p>Bạn đã đặt hàng thành công, vui lòng quay lại trang chủ để tiếp tục mua sắm</p>
+                <p>Đơn hàng sẽ được gửi đi trong thời gian sớm nhất, xin cảm ơn!</p>
+                <br />
                 <button className="cartcheckout__suceesshow-button" onClick={props.redirect}>
                     Quay về trang chủ
                 </button>
             </div>
-        </div>:<></>}
-        {props.fall?<div className="blur">
+        </div> : <></>}
+        {props.fall ? <div className="blur">
             <div className="cartcheckout__successshow">
-            <i class="fa-solid fa-circle-check"></i>
-            <h3>Đặt hàng thất bại!</h3>
-            <p>Vui lòng kiểm tra lại thông tin đơn hàng</p>
-            <br/>
+                <i class="fa-solid fa-circle-check"></i>
+                <h3>Đặt hàng thất bại!</h3>
+                <p>Vui lòng kiểm tra lại thông tin đơn hàng</p>
+                <br />
                 <button className="cartcheckout__suceesshow-button" onClick={props.setFall}>
                     Quay về trang chủ
                 </button>
             </div>
-        </div>:<></>}
+        </div> : <></>}
     </div>
 }
