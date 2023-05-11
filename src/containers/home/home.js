faa9f8d6bf26fb761d36406799e9c3f642e9fa15
Nguyen Van Hanh <nguyenhanh3@telio.vn>
2023-05-11 14:15:19 +0700

    fix: search item

--- src/containers/home/home.js
+++ src/containers/home/home.js
@@ -2,25 +2,36 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { SERVER } from "../../util";
 import HomeComponent from "./component/home";
-export default function HomeContainer(props){
-    const [categoryList,setCategoryList]=useState([0]);
-    const [listItem,setListItem]=useState([]);
-    useEffect(()=>{
-        axios.get(SERVER+'/category')
-        .then(item=>item.data)
-        .then(item=>{
-            setCategoryList(item);
+import { useLocation } from "react-router-dom/cjs/react-router-dom.min";
+export default function HomeContainer(props) {
+    const [categoryList, setCategoryList] = useState([0]);
+    const [listItem, setListItem] = useState([]);
+    const { search } = useLocation();
+    const keySearch = search.split('=')?.[1] || ''
+
+    useEffect(() => {
+        axios.get(SERVER + '/category')
+            .then(item => item.data)
+            .then(item => {
+                setCategoryList(item);
+            })
+    }, [])
+
+    useEffect(() => {
+        axios.get(SERVER + '/item', {
+            params: {
+                keySearch
+            }
         })
-        axios.get(SERVER+'/item')
-        .then(item=>item.data)
-        .then(item=>{
-            setListItem(item);
-        })
-        .catch(item=>{
-            alert('Lỗi thực hiện request, vui lòng thử lại');
-        })
-    },[])
+            .then(item => item.data)
+            .then(item => {
+                setListItem(item);
+            })
+            .catch(item => {
+                alert('Lỗi thực hiện request, vui lòng thử lại');
+            })
+    }, [keySearch])
     return <>
-        {listItem.length>0 && <HomeComponent categoryList={categoryList} listItem={listItem}/>}
+        {listItem.length > 0 && <HomeComponent categoryList={categoryList} listItem={listItem} />}
     </>
 }
--- src/shared/layout/navbar/navbar.js
+++ src/shared/layout/navbar/navbar.js
@@ -1,26 +1,26 @@
 import NavbarComponent from "./component/navbar";
-import React,{ useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch } from "react-redux";
 import { timkiem } from "../../store/actions";
 import { SERVER } from "../../../util";
-export default function NavbarContainer(props){
-    const [category,setCategory]=useState([]);
-    const [searchKey,setSearchKey]=useState("");
-    const dispatch=useDispatch();
-    useEffect(()=>{
-        fetch(SERVER+'/category')
-        .then(item=>item.json())
-        .then(item=>{
-            setCategory(item);
-        });
-    },[])
-    const handleSearchKey=(item)=>{
+export default function NavbarContainer(props) {
+    const [category, setCategory] = useState([]);
+    const [searchKey, setSearchKey] = useState("");
+    const dispatch = useDispatch();
+    useEffect(() => {
+        fetch(SERVER + '/category')
+            .then(item => item.json())
+            .then(item => {
+                setCategory(item);
+            });
+    }, [])
+    const handleSearchKey = (item) => {
         setSearchKey(item.target.value);
     }
-    const search=()=>{
-        dispatch(timkiem(searchKey));
+    const search = () => {
+        window.location = searchKey ? `/home?keySearch=${searchKey}` : '/home'
     }
     return <>
-        <NavbarComponent category={category} handleSearchKey={handleSearchKey} search={search}/>
+        <NavbarComponent category={category} handleSearchKey={handleSearchKey} search={search} />
     </>
 };
